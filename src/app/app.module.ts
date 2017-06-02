4e00b13a0bc0a5614b3e0c05592ad18cd7338823
Phil Moir <pgmoir@gmail.com>
2017-06-02 13:18:37 +0100

    Basic routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { MytflService } from './mytfl/mytfl.service';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TubelinesComponent } from './tubelines/tubelines.component';
@@ -17,6 +18,25 @@ import { TubelineListComponent } from './tubelines/tubeline-list/tubeline-list.c
 import { TubelineDetailComponent } from './tubelines/tubeline-detail/tubeline-detail.component';
 import { TubelineItemComponent } from './tubelines/tubeline-list/tubeline-item/tubeline-item.component';
 import { MytflComponent } from './mytfl/mytfl.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+
+import { MytflService } from './mytfl/mytfl.service';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tube', component: TubelinesComponent, children: [
+    { path: ':name', component: TubelineDetailComponent }
+  ] },
+  { path: 'bus', component: BusroutesComponent },
+  { path: 'road', component: RoadsComponent },
+  { path: 'river', component: RiverroutesComponent },
+  { path: 'cablecar', component: CablecarsComponent },
+  { path: 'rail', component: RaillinesComponent },
+  { path: 'mytfl', component: MytflComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
   declarations: [
@@ -32,12 +52,15 @@ import { MytflComponent } from './mytfl/mytfl.component';
     TubelineListComponent,
     TubelineDetailComponent,
     TubelineItemComponent,
-    MytflComponent
+    MytflComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [MytflService],
   bootstrap: [AppComponent]
